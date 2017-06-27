34eda9397986c17e44d53a858a26fae5ad137a7f
Dennis Majvall <dennis.majvall.sp13@thegameassembly.com>
2017-06-27 23:02:02 +0200

    added a search-bar for filtering the anime-list

--- www/js/anime-list.js
+++ www/js/anime-list.js
@@ -7,6 +7,23 @@ class AnimeList {
       });
 
       $('.anime-list').empty().template('anime-list', { shows: shows });
+      $('#searchInput').off().on('keyup', (e)=>{ this.onKeyUp($(e.target));});
+
+      this.aTags = {};
+      let me = this;
+      $('#search-list').find('a').each(function(){
+        me.aTags[$(this).text().toLowerCase()] = $(this);
+      });
     });
+  }
+
+  onKeyUp(el){
+    let s = el.val().toLowerCase();
+    for(let key in this.aTags){
+      if (key.indexOf(s) != -1)
+        this.aTags[key].removeClass('hidden');
+      else
+        this.aTags[key].addClass('hidden');
+    }
   }
 }
